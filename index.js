90d6f8fe8eb265e5c16cdd66689aaa108d39d0dc
Replit user <>
2023-06-09 11:57:30 -0300

    Training

--- index.js
+++ index.js
@@ -19,9 +19,20 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  const dateStr = req.params.date;
+  const seconds = Number(dateStr);
+  let date;
+  if (isNaN(seconds)) {
+    date = new Date(dateStr);
+  } else {
+    date = new Date(seconds);
+  }
+  const result = {
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  };
+  res.json(result);
 });
 
 
